2b8b0c4488d362688a60e78fc259e07cdc92ffde
Nadiyaghefira109 <withnadiya@gmail.com>
2026-06-30 23:25:53 +0700

    Perbaiki konfigurasi CORS tanpa slash di origin

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: "https://cari-makan-frontend.vercel.app/", // atau bisa diganti dengan domain frontend kamu // atau bisa diganti dengan domain frontend kamu
+    origin: "https://cari-makan-frontend.vercel.app", // atau bisa diganti dengan domain frontend kamu // atau bisa diganti dengan domain frontend kamu
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
